e5de3a2cc0bc0da0f07e210ef25e44e6645ac752
liyj144 <liyj144@163.com>
2017-03-11 00:22:33 +0800

    添加grach数据批量导入脚本

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from DataManager import DataManager
 if __name__ == '__main__':
     dm = DataManager()
     # dm.dropResult()
-    dm.createDb()
+    # dm.createDb()
     # dm.deleteResult()
-    dm.manageDataCore(173889)
+    # dm.manageDataCore(173889)
+    dm.computeArch()
 
